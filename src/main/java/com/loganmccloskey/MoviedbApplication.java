f84e929bd367a961a0ce642aaea1352fd65652ab
lamccloskey <lamccloskey@gmail.com>
2016-08-16 16:07:21 -0500

    setup gulpfile

--- src/main/java/com/loganmccloskey/MoviedbApplication.java
+++ src/main/java/com/loganmccloskey/MoviedbApplication.java
@@ -20,7 +20,6 @@ public class MoviedbApplication implements CommandLineRunner {
 	public static void main(String[] args) {
 		SpringApplication.run(MoviedbApplication.class, args);
 	}
-	
 
 	@Override
 	public void run(String... args) throws Exception {
