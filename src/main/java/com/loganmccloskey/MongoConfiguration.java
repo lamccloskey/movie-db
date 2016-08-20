6e3d578f0cc128955c5e24f75c08e4c3a4eefa08
lamccloskey <lamccloskey@gmail.com>
2016-08-20 15:18:01 -0500

    minor clean up

--- src/main/java/com/loganmccloskey/MongoConfiguration.java
+++ src/main/java/com/loganmccloskey/MongoConfiguration.java
@@ -1,6 +1,5 @@
 package com.loganmccloskey;
 
-import org.apache.log4j.spi.LoggerFactory;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
--- src/main/resources/static/app/core/env.js
+++ src/main/resources/static/app/core/env.js
@@ -8,7 +8,7 @@
     window.__env.enableDebug = false;
 
     //TMBD
-    window.__env.apiUrl = 'https://api.themoviedb.org/3/';
+    window.__env.apiUrl = 'https://api.themoviedb.org/3';
     window.__env.apiKey = '2070102b343bf69650bde4b0063cd085';
 
 })(this);
