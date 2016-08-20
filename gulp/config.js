37f07bb1728f18e0c2bc73e5adb38dcd64a73713
lamccloskey <lamccloskey@gmail.com>
2016-08-20 15:08:07 -0500

    env

--- gulp/config.js
+++
@@ -1,8 +0,0 @@
-'use-strict';
-
-module.exports = {
-    rootPath: 'src/main/resources/static/',
-    srcPath: 'src/main/resources/static/app/',
-    destPath: 'src/main/resources/static/build/',
-    bowerPath: './bower',
-};
